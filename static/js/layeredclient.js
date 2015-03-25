9540b4e233aff666d89d03a63e49054b4f8d2dbb
John Walker <john.lou.walker@gmail.com>
2015-03-25 15:09:23 -0400

    Add index.html and layeredclient.js

---
+++ static/js/layeredclient.js
@@ -0,0 +1,7 @@
+var viewer = OpenSeadragon({
+    id:            'foo',
+    prefixUrl:     'openseadragon/images/',
+    tileSources:   'dzi/2010/ChadGospels-142-138-2010.dzi',
+    maxZoomPixelRatio: 1000
+});
+
