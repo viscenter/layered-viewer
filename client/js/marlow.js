677a539ffbb6bfffcc8fcba6c9a5020d1aa2d5e3
John L. Walker <john.lou.walker@gmail.com>
2015-04-05 19:24:58 -0400

    Add two synchronized OSD clients

---
+++ client/js/marlow.js
@@ -0,0 +1,47 @@
+(function() {
+    var primary = OpenSeadragon({
+        id: "primary",
+        prefixUrl: "/build/openseadragon/images/",
+        tileSources: "/test/data/testpattern.dzi",
+        showNavigator: false
+    });
+    var secondary = OpenSeadragon({
+        id: "secondary",
+        prefixUrl: "/build/openseadragon/images/",
+        tileSources: "/test/data/testpattern.dzi",
+        showNavigator: false
+    });
+    primary.addHandler('pan', function (e) {
+        if (primary && secondary) {
+            if (secondary.viewport)
+                secondary.viewport.panTo(primary.viewport.getCenter(false));
+        }
+    });
+    primary.addHandler('zoom', function (e) {
+        if (primary && secondary) {
+            if (secondary.viewport) {
+                secondary.viewport.panTo(primary.viewport.getCenter(false));
+                secondary.viewport.zoomTo(primary.viewport.getZoom(false));
+            }
+        }
+    });
+    var mpos = {};
+    var pc = primary.canvas.children[0];
+    var ctx = pc.getContext("2d");
+
+    function getmpos(canvas, e) {
+        var rect = canvas.getBoundingClientRect();
+        return { x: e.clientX - rect.left,
+		 y: e.clientY - rect.top };
+    }
+    pc.addEventListener('mousemove', function (e) {
+        mpos = getmpos(pc, e);
+        if (primary.viewport != undefined) {
+            primary.forceRedraw();
+            ctx.clearRect(mpos.x,mpos.y, 50,50);
+        }
+    });
+    primary.addHandler('tile-drawn', function (e) {
+        ctx.clearRect(mpos.x,mpos.y,50,50);
+    });
+}());
