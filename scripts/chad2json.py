2cc5f75b6871db7b8f6dd4f2128ba4feed971a69
Stephen Parsons <stephen.r.parsons@gmail.com>
2015-04-06 15:25:14 -0400

    add chad json file and script

---
+++ scripts/chad2json.py
@@ -0,0 +1,32 @@
+# made only for chad gospels
+# run from /data/images
+
+import json, os
+
+directories = os.listdir('.')
+directories.remove(__file__)
+
+images = {}
+for directory in directories:
+    for filename in os.listdir(directory):
+        filename = filename.split('.')[0]
+        volumeID, pageNumInsideVersion, folioID, version = filename.split('-')
+        if folioID not in images:
+            images[folioID] = []
+        images[folioID].append([version, "data/dzi/"+filename+".dzi"])
+
+names = sorted(images.keys())
+for name in names:
+    images[name] = sorted(images[name])
+names.insert(0, names.pop())
+names.insert(0, names.pop())
+names.insert(0, names.pop(-2))
+names.insert(0, names.pop())
+names.insert(len(names), names.pop(240))
+names.insert(len(names), names.pop(240))
+
+final = []
+for name in names:
+    final.append([name, images[name]])
+
+open("chad.json", "w").write(json.dumps(final, indent=4, separators=(',',': ')))
