fa99f89163187c680af5df8c1f550256f2269496
Stephen Parsons <stephen.r.parsons@gmail.com>
2015-04-06 14:36:47 -0400

    remove old static directory

--- static/js/layeredclient.js
+++
@@ -1,7 +0,0 @@
-var viewer = OpenSeadragon({
-    id:            'foo',
-    prefixUrl:     'openseadragon/images/',
-    tileSources:   'dzi/2010/ChadGospels-142-138-2010.dzi',
-    maxZoomPixelRatio: 1000
-});
-
